e89cad59b508c7f561df3c26c97307d0fba95b57
Luca Ravasio <luca.ravasio.1@gmail.com>
2021-04-09 17:03:13 +0200

    Changes to be committed: 	modified:   setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='oopygame',
-    version='0.1.1',    
+    version='0.2.1',    
     description='An object oriented API based on pygame',
     url='https://github.com/Py-GNU-Unix/oopygame',
     author='Py-GNU-Unix',
@@ -10,16 +10,16 @@ setup(
     license='GPL-3.0',
     packages=['oopygame'],
     install_requires=['pygame>=2.0',
-                      'CairoSVG',                     
+                      'CairoSVG',
                       ],
 
-    classifiers=[  
-        'Operating System :: POSIX :: Linux',        
+    classifiers=[
+        'Operating System :: POSIX :: Linux',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
     ],
-    
+
     include_package_data=True
 )
