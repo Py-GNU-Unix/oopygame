5c967b6f612307f2a3b02f4d5c831a54eec008d8
Luca Ravasio <luca.ravasio.1@gmail.com>
2021-04-10 14:02:56 +0200

    Updating README.md: 	modified:   screenshot.png 	modified:   test_oopygame.py

--- test_oopygame.py
+++ test_oopygame.py
@@ -14,19 +14,17 @@
 # ~ along with oopygame.  If not, see <https://www.gnu.org/licenses/>.
 
 import oopygame as oop
-import pygame
 
-if __name__ == "__main__":
-    W = oop.Window(flags=pygame.DOUBLEBUF, bg_color=oop.colors.white)
-    clock = oop.time.Clock(FPS=120, window=W)
-    obj = oop.Object(W, pos=(0, 250))
+W = oop.Window(flags=pygame.DOUBLEBUF, bg_color=oop.colors.white)
+clock = oop.time.Clock(FPS=120, window=W)
+obj = oop.Object(W, pos=(0, 250))
 
-    restart_pos = (0-obj.get_image().get_width(), 250)
+restart_pos = (0-obj.get_image().get_width(), 250)
 
-    while True:
-        obj.move_right(4)
-        if obj.is_out_of_window() and not obj.is_touching_borders():
-            obj.set_pos(restart_pos)
+while True:
+    obj.move_right(4)
+    if obj.is_out_of_window() and not obj.is_touching_borders():
+        obj.set_pos(restart_pos)
 
-        clock.tick()
-        W.do_routine()
+    clock.tick()
+    W.do_routine()
