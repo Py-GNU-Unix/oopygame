0036e0f1d39e48105827a2b9d572bd47014a0381
Luca Ravasio <luca.ravasio.1@gmail.com>
2021-04-10 14:04:27 +0200

    Version Name Updated: 	modified:   setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='oopygame',
-    version='0.2.1',    
+    version='0.2.2',    
     description='An object oriented API based on pygame',
     url='https://github.com/Py-GNU-Unix/oopygame',
     author='Py-GNU-Unix',
